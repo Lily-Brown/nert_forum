fd49375b8c01050a05971a5302223552c19d0801
LilyCole <Lilia.Cole@gmail.com>
2016-11-18 13:52:42 -0800

    adds comment migration

---
+++ nert_forum/db/migrate/20161118211618_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :comments do |t|
+      t.string :text_body
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :post, foreign_key: true
+      # t.belongs_to :event, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
