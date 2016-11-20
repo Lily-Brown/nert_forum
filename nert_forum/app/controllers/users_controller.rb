f25bc61bb86f570b15bd9ad005f5e95333ba041c
LilyCole <Lilia.Cole@gmail.com>
2016-11-19 20:22:12 -0800

    adds edit/update methods to user_controller and links to edit/delete to user#show view

--- nert_forum/app/controllers/users_controller.rb
+++ nert_forum/app/controllers/users_controller.rb
@@ -14,12 +14,26 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = 'Changes saved successful'
+      redirect_to @user
+    else
+      flash[:error] = @user.error.full_messages.join(' ')
+      render :edit
+    end
   end
 
   def destroy
   end
 
+  private 
+
+  def user_params
+    params.require(:user).permit(:name,:image,:neighborhood)
+  end
 end
