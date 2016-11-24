fc4935593bc680de5ff2bcde9d62e2af54030600
LilyCole <Lilia.Cole@gmail.com>
2016-11-24 15:31:32 -0800

    adds verify_admin for users#new, create, index and destroy

--- nert_forum/app/controllers/users_controller.rb
+++ nert_forum/app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   load_and_authorize_resource
   before_action :get_user, only: [:show, :edit, :update, :destroy]
   before_action :verify_user, only: [:edit, :update]
+  before_action :verify_admin, only: [:new, :create, :index, :destroy]
 
   def index
     @users = User.all
@@ -52,7 +53,14 @@ class UsersController < ApplicationController
   def verify_user
     unless @user === current_user
       flash[:error] = 'You are not authorized to perform this action.'
-      redirect_to :back
+      redirect_to root_path
+    end
+  end
+
+  def verify_admin
+    unless current_user.admin
+      flash[:error] = 'You must be an adminstrator to perform this action.'
+      redirect_to root_path
     end
   end
 
