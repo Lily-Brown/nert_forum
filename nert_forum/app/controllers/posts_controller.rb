f6cfedca4ef45af02d8b3d743898dccdf769fdac
LilyCole <Lilia.Cole@gmail.com>
2016-11-24 09:22:45 -0800

    adds posts#destroy with verify_user and get_post before actions

--- nert_forum/app/controllers/posts_controller.rb
+++ nert_forum/app/controllers/posts_controller.rb
@@ -1,20 +1,19 @@
 class PostsController < ApplicationController
+  before_action :get_post, only: [:show, :edit, :update, :destroy]
+  before_action :verify_user, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
     @new_comment = Comment.new
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update_attributes(post_params)
 
     if @post.save
@@ -26,7 +25,29 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+
+    if @post.destroy
+      flash[:success] = "Post deleted successfully."
+      redirect_to posts_path
+    else
+      flash[:error] = "Post has not been deleted."
+      redirect_to @post
+    end
+  end
+
   private
+
+  def get_post
+    @post = Post.find(params[:id])
+  end
+
+  def verify_user
+    unless @post.owner == current_user
+      flash[:error] = 'You are not authorized to perform this action.'
+      redirect_to :back
+    end
+  end
 
   def post_params
     params.require(:post).permit(:title,:text_body,:user_id)
