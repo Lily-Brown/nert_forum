1a7539bbeed16b8707872575cd12dab14fb87b00
LilyCole <Lilia.Cole@gmail.com>
2016-11-24 12:02:39 -0800

    adds cancan and defaults admin to false

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :email, presence: true
+
+  before_save :default_values
+  def default_values
+    self.admin ||= false
+  end
 
   # Relationships
   has_many :posts
