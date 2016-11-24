aab86a2c1d6f3223ab4591de0b5ad94675d7a97a
LilyCole <Lilia.Cole@gmail.com>
2016-11-24 09:13:22 -0800

    adds posts#edit method and view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,27 @@ class PostsController < ApplicationController
     @new_comment = Comment.new
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(post_params)
+
+    if @post.save
+      flash[:success] = "Post updated successfully."
+      redirect_to @post
+    else
+      flash[:error] = "Post has not been updated."
+      render :edit
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title,:text_body,:user_id)
+  end
+
 end
