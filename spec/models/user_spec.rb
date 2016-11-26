48b6b661e68efb05251ca064edd2079dba4e3981
LilyCole <Lilia.Cole@gmail.com>
2016-11-26 15:21:35 -0800

    updates user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,17 +16,57 @@ RSpec.describe User, type: :user do
     end
   end
 
-  # describe "::text_body" do
-  #   it "has an text_body attribute" do
-  #     user.text_body = "This is the body of my user."
-  #     expect(user.text_body).to eq("This is the body of my user.")
-  #   end
-  # end
+  describe "::email" do
+    it "has an email attribute" do
+      user.email = "email@email.com"
+      expect(user.email).to eq("email@email.com")
+    end
+  end
 
-  # describe "::owner" do
-  #   it "can belong to a user" do
-  #     expect(user.owner).to eq(user)
-  #   end
-  # end
+  describe "::neighborhood" do
+    it "has an neighborhood attribute" do
+      user.neighborhood = "Neighborhood"
+      expect(user.neighborhood).to eq("Neighborhood")
+    end
+  end
+
+  describe "::comments" do
+    it "can have many comments" do
+      user = User.create(email:"test@test.com",password:"password")
+      event = user.events.create(name:"Event Name",description:"This is the description for the Event.",location:"Event Location.")
+      comment1 = event.comments.create(text_body:"This is the body of my comment.",user_id:user.id)
+      comment = event.comments.create(text_body:"This is the body of my comment.",user_id:user.id)
+      expect(user.comments).to eq([comment1,comment])
+    end
+  end
+
+  describe "::posts" do
+    it "can have many posts" do
+      user = User.create(email:"test@test.com",password:"password")
+      post = user.posts.create(title:"Post Name",text_body:"This is the text for the Post.")
+      post1 = user.posts.create(title:"Post Name",text_body:"This is the text for the Post.")
+      expect(user.posts).to eq([post,post1])
+    end
+  end
+
+  describe "::events" do
+    it "can have many events" do
+      user = User.create(email:"test@test.com",password:"password")
+      event = user.events.create(name:"Event Name",description:"This is the description for the Event.",location:"Event Location.")
+      event1 = user.events.create(name:"Event Name",description:"This is the description for the Event.",location:"Event Location.")
+      expect(user.events).to eq([event,event1])
+    end
+  end
+
+  describe "::attended_events" do
+    it "can have many attended_events" do
+      user = User.create(email:"test@test.com",password:"password")
+      event = user.events.create(name:"Event Name",description:"This is the description for the Event.",location:"Event Location.")
+      event1 = user.events.create(name:"Event Name",description:"This is the description for the Event.",location:"Event Location.")
+      event.attendees << user
+      event1.attendees << user
+      expect(user.attended_events).to eq([event,event1])
+    end
+  end
 
 end
