ce7d44cb65aea4a7211c0255b64bef1ac2db3085
LilyCole <Lilia.Cole@gmail.com>
2016-11-26 15:07:24 -0800

    adds test for events_user model

--- spec/models/events_user_spec.rb
+++ spec/models/events_user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe EventsUser, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe "::user" do
+    it "can belong to a user" do
+      user = User.create(email:"test@test.com",password:"password")
+      event = user.events.create(name:"Event Name",description:"This is the description for the Event.",location:"Event Location.")
+      attendee = EventsUser.create(user_id:user.id,event_id:event.id)
+      expect(attendee.user).to eq(user)
+    end
+  end
+
+    describe "::event" do
+    it "can belong to a event" do
+      user = User.create(email:"test@test.com",password:"password")
+      event = user.events.create(name:"Event Name",description:"This is the description for the Event.",location:"Event Location.")
+      attendee = EventsUser.create(user_id:user.id,event_id:event.id)
+      expect(attendee.event).to eq(event)
+    end
+  end
 end
