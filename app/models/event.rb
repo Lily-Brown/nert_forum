03b634feefefe0b3168fa7a72539791088887356
LilyCole <Lilia.Cole@gmail.com>
2016-11-25 17:36:42 -0800

    adds events_users table and sets up relationships in models

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,9 @@ class Event < ApplicationRecord
   alias_method :owner, :user
   
   has_many :comments, as: :parent
+
+  has_many :events_users, dependent: :destroy
+  has_many :attendees, through: :events_users, source: :user
+  # has_many :users, through: :events_users
+  # alias_method :users, :attendees
 end
