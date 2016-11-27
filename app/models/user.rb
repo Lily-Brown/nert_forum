4313f7c30458e99141014f740ad1a7519dbce119
LilyCole <Lilia.Cole@gmail.com>
2016-11-26 17:42:22 -0800

    adds photo attribute to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_attached_file :photo, styles: { medium: '300x300#', thumb: '100x100#' }, default_url: '/images/:style/missing.png'
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
+
   validates :email, presence: true
 
   before_save :default_values
