7d79306f84b2245a56c6e880c26759217e5880dd
LilyCole <Lilia.Cole@gmail.com>
2016-11-28 23:00:15 -0800

    tries another image path

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :photo, styles: { medium: '300x300#', thumb: '100x100#' }, default_url: '/:style/user_default.png'
+  has_attached_file :photo, styles: { medium: '300x300#', thumb: '100x100#' }, default_url: 'app/assets/images/:style/user_default.png'
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 
   validates :email, presence: true
